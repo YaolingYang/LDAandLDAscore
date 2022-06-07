74b7fb6ac8b935a8e052474e7664413754d56be4
Yaoling Yang <91947158+YaolingYang@users.noreply.github.com>
2022-06-07 18:13:09 +0100

    Update LDA score QC.R

--- LDA score QC.R
+++ LDA score QC.R
@@ -1,6 +1,6 @@
 #LDA score plot after quality control
 LDAS_plot_QC <- function(i){
-  #setwd(paste0("C:/Ubuntu/LDAall/chr",i))
+
   LDA_score<-read.table(paste0('LDA_score_maxmin_chr',i,'.csv'),sep=',',header=TRUE)
   
   LDA_score <- cbind(data.frame(pd=LDA_score$pd,LDAS=LDA_score$LDAS,error=LDA_score[,5]-LDA_score[,6]),
@@ -16,8 +16,6 @@ LDAS_plot_QC <- function(i){
     LDA_score <- LDA_score[-remove_index,]
   }
   
-  setwd("C:/Ubuntu/LDAall")
-  #png(paste0('LDAS_chr',i,'_QC.png'),width=1600,height=700)
   ggplot(LDA_score,aes(x=pd/1000000,y=LDAS))+geom_point(size=1)+
     xlab(paste('Chromosome',i,'position hg19 GRCh37 (Mb)')) + ylab ('LDA score')+
     theme(axis.title.x = element_text(size=15),
